72ce1c81adb6badb5de5711dd91cd3303622cb01
Jacob Patterson <jacob@jacobpa.com>
2021-01-15 22:42:59 -0500

    fish: Clean up config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,11 @@ set -g theme_title_display_process 'yes'
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
-end
 
-set BASE16_KITTY "$HOME/.config/kitty/base16-kitty/"
-if status --is-interactive; and test -n "$BASE16_THEME" -a -d "$BASE16_KITTY/colors"; 
-    kitty @set-colors $BASE16_KITTY/colors/base16-$BASE16_THEME-256.conf
+    set BASE16_KITTY "$HOME/.config/kitty/base16-kitty/"
+    if test -n "$BASE16_THEME" -a -d "$BASE16_KITTY/colors"
+        kitty @set-colors $BASE16_KITTY/colors/base16-$BASE16_THEME-256.conf
+    end
 end
 
 if command -q rbenv; and status --is-interactive
